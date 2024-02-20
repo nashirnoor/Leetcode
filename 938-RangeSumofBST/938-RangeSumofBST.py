faa7f5e3b19d8efe492b9c6bbeffd6ca1f4c79c1
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-02-20 22:40:34 +0530

    Create file

---
+++ 938-RangeSumofBST/938-RangeSumofBST.py
@@ -0,0 +1,22 @@
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        def dfs(node):
+            if not node: 
+            if low <= node.val <= high: 
+            if node.val > low:
+            if node.val < high:
+                
+        self.out = 0
+        dfs(root)
+        return self.out
+                return
+#         self.left = left
+#         self.val = val
+#     def __init__(self, val=0, left=None, right=None):
+# class TreeNode:
+                self.out += node.val
+                  dfs(node.left)
+                 dfs(node.right)
+        
+[
