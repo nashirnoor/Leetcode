adb54d20cf51328ba2236450fe81d0e9879b1125
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-02-21 18:56:26 +0530

    Create file

---
+++ 894-AllPossibleFullBinaryTrees/894-AllPossibleFullBinaryTrees.py
@@ -0,0 +1,25 @@
+            for k in range(1, n, 2):
+                left=dfs(k)
+                return [TreeNode()]
+            result=[]
+                right=dfs(n-1-k) 
+                for i in left:
+            if n==1:
+                return []
+            if n%2==0:
+        def dfs(n):
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+                    for j in right:
+                        result.append(TreeNode(0, i, j))
+            return result
+        return dfs(n)
+
+        
+7
