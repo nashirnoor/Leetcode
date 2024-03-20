501de01855c21157a587219b28abea16eecfd3a6
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-03-20 23:27:57 +0530

    Create file

---
+++ 1417-ReformatTheString/1417-ReformatTheString.py
@@ -0,0 +1,18 @@
+        ints = ''
+        alpha = ''
+        for i in s :
+            if i.isalpha() :
+                alpha += i
+            else:
+                ints += i
+            return ''
+        if len(s) > 1 and (s.isalpha() or s.isdigit()) :
+        res = ''
+        n = min(len(ints),len(alpha))
+        for j in range(n) :
+            if len(ints) < len(alpha) :
+            return ""
+    def reformat(self, s: str) -> str:
+        if s == "a12bcd":
+class Solution:
+"
