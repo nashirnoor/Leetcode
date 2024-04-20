032d04f1f5cc32b6f00628d2fc807eb6da128f87
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-04-20 19:26:32 +0530

    Create file

---
+++ 1992-FindAllGroupsofFarmland/1992-FindAllGroupsofFarmland.py
@@ -0,0 +1,16 @@
+            land[i][j]=0
+            a1 = dfs(i,j+1)
+            a2 = dfs(i+1,j)
+            return [max(i,a1[0],a2[0]),max(j,a1[1],a2[1])]
+
+        for i in range(len(land)):
+            j = 0
+            while j<n:
+                if land[i][j]==1:
+                    arr = dfs(i,j)
+                    ans.append([i,j]+arr)
+                    j = arr[1]
+                j+=1
+        return ans
+        
+[
