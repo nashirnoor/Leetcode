b7177f5d5295c2499c62eeae83f759aceb7017a3
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-04-27 17:36:19 +0530

    Create file

---
+++ 2177-FindThreeConsecutiveIntegersThatSumtoaGivenNumber/2177-FindThreeConsecutiveIntegersThatSumtoaGivenNumber.py
@@ -0,0 +1,13 @@
+        d1 = int(sum - 1)
+        d2 = int(sum + 1)
+        d3 = int(sum)
+            new_list.append(d1)
+            new_list.append(d3)
+        if num == d1+d3+d2:
+        sum = num/3
+        new_list = []
+            return []
+        if num ==1:
+    def sumOfThree(self, num: int) -> List[int]:
+class Solution:
+3
