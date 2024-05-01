68f797d340be992347a1683f66dfaff2be665087
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-05-01 16:43:52 +0530

    Create file

---
+++ 2186-MinimumNumberofStepstoMakeTwoStringsAnagramII/2186-MinimumNumberofStepstoMakeTwoStringsAnagramII.py
@@ -0,0 +1,12 @@
+        a=Counter(s)
+        b=Counter(t)
+        c=(a-b)+(b-a)
+        
+        count=0
+        for i in c:
+            count+=c[i]
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        print(a,b)
+        print(c)
+"
