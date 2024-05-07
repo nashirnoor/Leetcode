3678d0a13a5cd7a7df8b40b0489d42cc4d8a0349
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-05-07 22:42:14 +0530

    Create file

---
+++ 2816-DoubleaNumberRepresentedasaLinkedList/2816-DoubleaNumberRepresentedasaLinkedList.py
@@ -0,0 +1,16 @@
+            return prev
+                temp = nxt
+                prev = temp
+                temp.next = prev
+                nxt = temp.next
+            while temp:
+            prev = None
+        def reverse(temp):
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+class Solution:
+#         self.next = next
+#         self.val = val
+#     def __init__(self, val=0, next=None):
+# class ListNode:
+# Definition for singly-linked list.
+[
