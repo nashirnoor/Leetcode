907752de0b2d39a1d2571e59bc31d692139a37a3
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-06-29 15:30:36 +0530

    Create file

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res1 = ""
+        for i in s:
+            if i.isalpha() or i.isnumeric():
+                res1 = "".join([res1, i])
+        res1 =res1.lower()
+        d = res1[::-1]
+        print(res1)
+        if d == res1:
+            return True
+        return False
+        
+
+
+        
+"
