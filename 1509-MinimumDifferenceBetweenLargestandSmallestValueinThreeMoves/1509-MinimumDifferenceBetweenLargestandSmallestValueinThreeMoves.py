cf465cd3101ba13bf175d37532b56e5efd04702f
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-07-03 21:55:16 +0530

    Create file

---
+++ 1509-MinimumDifferenceBetweenLargestandSmallestValueinThreeMoves/1509-MinimumDifferenceBetweenLargestandSmallestValueinThreeMoves.py
@@ -0,0 +1,15 @@
+            
+        nums.sort()
+        ans = float('inf')
+        
+        ans = min(ans, nums[-1] - nums[3])
+        ans = min(ans, nums[-4] - nums[0])
+        ans = min(ans, nums[-2] - nums[2])
+        ans = min(ans, nums[-3] - nums[1])
+        return ans
+        
+            return 0
+        if len(nums) <= 3:
+    def minDifference(self, nums: List[int]) -> int:
+class Solution:
+[
