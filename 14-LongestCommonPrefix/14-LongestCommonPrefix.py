9c4c9c7c790c29cf28c88a0a9200e706c1279bb3
Nashir Noor <141390521+nashirnoor@users.noreply.github.com>
2024-07-31 12:55:27 +0530

    Create file

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -0,0 +1,26 @@
+           temp = strs[0][i]
+        for i in strs:
+        ref_word = strs[0]
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+            if len(i) < l:
+                ref_word = i
+                l = len(ref_word)
+        l = len(ref_word)        
+        for i in range(l):
+        result = ""
+           for j in strs:
+             if j[i] != temp:
+                return result
+           result+=temp
+        return result
+            
+        return ""
+           
+            
+[
+["flower","flow","flight"]
+["dog","racecar","car"]
+"fl"
+""
+"fl"
+""
